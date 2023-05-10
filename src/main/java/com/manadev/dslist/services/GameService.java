58691c81e437bf391da40707613ff509c5ccfa2b
ManassesM <manamiui2001@gmail.com>
2023-05-10 10:28:00 -0300

    GET game by id

--- src/main/java/com/manadev/dslist/services/GameService.java
+++ src/main/java/com/manadev/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.manadev.dslist.services;
 
+import com.manadev.dslist.dto.GameDTO;
 import com.manadev.dslist.dto.GameMinDTO;
 import com.manadev.dslist.entities.Game;
 import com.manadev.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,8 +16,15 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
     }
 }
